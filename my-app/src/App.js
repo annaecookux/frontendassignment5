87af21994b791099030875b649da80cf185e3ce8
Anna E. Cook <54285604+annaecookux@users.noreply.github.com>
2022-11-12 12:36:47 -0600

    setup

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>Bengal Tigers</h1>
+  <p>Easily recognized by its coat of reddish-orange with dark stripes, the tiger is the largest wild cat in the world. The big cat's tail is three feet long. On average the big cat weighs 450 pounds, about the same as eight ten-year-old kids. It stands three feet tall with teeth four inches long and claws as long as house keys.</p>
+    </div>
+  );
+}
+
+export default App;
